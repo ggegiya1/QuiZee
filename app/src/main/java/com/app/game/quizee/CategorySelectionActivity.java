eb3f77845879d3805bcdc5acab3c1fd915373d0b
X102H17 <tristf7@gmail.com>
2017-04-06 17:33:32 -0400

    utilisation de liste pour le munu de choix des catégories

--- app/src/main/java/com/app/game/quizee/CategorySelectionActivity.java
+++ app/src/main/java/com/app/game/quizee/CategorySelectionActivity.java
@@ -1,25 +1,24 @@
 package com.app.game.quizee;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckedTextView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-/**
- * Created by ggegiya1 on 2/16/17.
- */
+public class CategorySelectionActivity extends AppCompatActivity {
 
-public class CategorySelectionActivity extends AppCompatActivity implements View.OnClickListener{
-
-    CheckedTextView Music;
-    CheckedTextView VideoGames;
-    CheckedTextView Computers;
-    CheckedTextView Geography;
-    CheckedTextView Art;
-    CheckedTextView GeneralKnowledge;
-    CheckedTextView History;
+    ListView categoryList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,107 +34,70 @@ public class CategorySelectionActivity extends AppCompatActivity implements View
             }
         });
 
+        categoryList = (ListView) findViewById(R.id.category_list);
 
 
-        Music = (CheckedTextView) findViewById(R.id.button_music);
-        Art = (CheckedTextView) findViewById(R.id.button_art);
-        Computers = (CheckedTextView) findViewById(R.id.button_computers);
-        History = (CheckedTextView) findViewById(R.id.button_history);
-        Geography = (CheckedTextView) findViewById(R.id.button_geography);
-        GeneralKnowledge = (CheckedTextView) findViewById(R.id.button_knowledge);
-        VideoGames = (CheckedTextView) findViewById(R.id.button_video_games);
+        //TODO obtaint category programmatically
+        int[] categoryImageId = new int[] {R.drawable.ic_geography, R.drawable.ic_computer, R.drawable.ic_art,
+                R.drawable.ic_general_knowledge, R.drawable.ic_art, R.drawable.ic_history};
+        String[] categories = new String[] {"Geography", "Computers", "Art","Art", "Bob", "History"};
 
-        Music.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (Music.isChecked())
-                    Music.setChecked(false);
-                else
-                    Music.setChecked(true);
-
-            }
-        });
-
-        Art.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (Art.isChecked())
-                    Art.setChecked(false);
-                else
-                    Art.setChecked(true);
-
-            }
-        });
-
-        Computers.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (Computers.isChecked())
-                    Computers.setChecked(false);
-                else
-                    Computers.setChecked(true);
-
-            }
-        });
-
-        History.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (History.isChecked())
-                    History.setChecked(false);
-                else
-                    History.setChecked(true);
-
-            }
-        });
-
-        Geography.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (Geography.isChecked())
-                    Geography.setChecked(false);
-                else
-                    Geography.setChecked(true);
-
-            }
-        });
-
-        GeneralKnowledge.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (GeneralKnowledge.isChecked())
-                    GeneralKnowledge.setChecked(false);
-                else
-                    GeneralKnowledge.setChecked(true);
-
-            }
-        });
-        VideoGames.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (VideoGames.isChecked())
-                    VideoGames.setChecked(false);
-                else
-                    VideoGames.setChecked(true);
-
-            }
-        });
-    }
-
-    @Override
-    public void onClick(View v) {
+        ContactAdapter adapterCategory = new ContactAdapter(this, categories, categoryImageId);
+        categoryList.setAdapter(adapterCategory);
 
     }
 
-    public void Play(View v) {
-        Intent intent = new Intent(getApplicationContext(), QuestionActivity.class);
-        intent.putExtra("Computers", Computers.isChecked());
-        intent.putExtra("History", History.isChecked());
-        intent.putExtra("Music", Music.isChecked());
-        intent.putExtra("VideoGames", VideoGames.isChecked());
-        intent.putExtra("Art", Art.isChecked());
-        intent.putExtra("GeneralKnowledge", GeneralKnowledge.isChecked());
-        intent.putExtra("Geography", Geography.isChecked());
-        intent.putExtra("mode", "categoriesPlay");
+    //contien temporairement un chekedTextView pour le réutiliser
+    private static class ViewHolder {
+        CheckedTextView categoryListItem;
+    }
+
+    //Adapter inspiré de
+    // http://www.androidinterview.com/android-custom-listview-with-image-and-text-using-arrayadapter/
+    private class ContactAdapter extends ArrayAdapter<String> {
+
+        String[] itemname;
+        int[] imgid;
+        Activity context;
+
+        public ContactAdapter (Activity context, String[] itemname, int[] imgid) {
+            super(context, R.layout.contacts_item_list_layout, itemname);
+            this.context=context;
+            this.itemname=itemname;
+            this.imgid=imgid;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater=context.getLayoutInflater();
+
+            final ViewHolder holder;
+
+            if(convertView == null) {
+                convertView = inflater.inflate(R.layout.category_selection_list_item, null, true);
+                holder = new ViewHolder();
+                holder.categoryListItem = (CheckedTextView) convertView.findViewById(R.id.category_item_list);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            holder.categoryListItem.setVisibility(View.VISIBLE);
+            holder.categoryListItem.setText(itemname[position]);
+            holder.categoryListItem.setCompoundDrawablesWithIntrinsicBounds(getDrawable(imgid[position]), null, null, null);
+            holder.categoryListItem.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    if(holder.categoryListItem.isChecked()) {
+                        holder.categoryListItem.setChecked(false);
+                    }   else {
+                        holder.categoryListItem.setChecked(true);
+                    }
+                }
+            }
+            );
+            return convertView;
+        }
     }
 }
