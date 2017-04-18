7502994ffac2f9c8f490d7ba42524760665a844d
X102H17 <tristf7@gmail.com>
2017-04-18 00:29:55 -0400

    -ajout dun summaire apres 10 questions (alertBuilder dialog)

--- app/src/main/java/com/app/game/quizee/QuestionActivity.java
+++ app/src/main/java/com/app/game/quizee/QuestionActivity.java
@@ -18,17 +18,18 @@ import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextSwitcher;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ViewSwitcher;
 
+import com.app.game.quizee.backend.Achievement;
 import com.app.game.quizee.backend.Category;
 import com.app.game.quizee.backend.Question;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
@@ -42,6 +43,11 @@ public class QuestionActivity extends AppCompatActivity{
     TextSwitcher answer2TextSwitcher;
     TextSwitcher answer3TextSwitcher;
     TextSwitcher answer4TextSwitcher;
+    ProgressBar pb;
+
+    List<TextSwitcher> TextSwitchers;
+    TextView category;
+    ImageView icon;
 
     //answer buttons
     Button correctlyAnswered;
@@ -51,20 +57,22 @@ public class QuestionActivity extends AppCompatActivity{
     ImageButton skipButton;
     ImageButton addTimeButton;
 
-    Boolean anserable;
-    String correctAnswer;
-    ProgressBar pb;
+    //game Attributes
     MyCountDownTimer countDownTimer;
-    List<TextSwitcher> TextSwitchers;
-    TextView category;
-    ImageView icon;
+    boolean gameEnded;
+    boolean anserable;
     int questionCount;
+    String correctAnswer;
 
+    int goodAnswers;
+    final int questionsPerGame = 10; //nombre de questions par parties
     final int baseTime = 10000; // temps entre les questions en milisecondes
-    final int preventTime = 1000; // temps entre les questions ou on ne peut pas clicker en milisecondes
+    final int preventTime = 300; // temps entre les questions ou on ne peut pas clicker en milisecondes
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         anserable = true;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
@@ -81,7 +89,6 @@ public class QuestionActivity extends AppCompatActivity{
         questionTextSwitcher = (TextSwitcher) findViewById(R.id.text_question);
         pb = (ProgressBar) findViewById(R.id.progressBar);
         pb.setRotation(180);
-        questionCount = -1;
 
         //ajoute les view a créé lors de lanimation de changement de texte du questionTextSwitcher
         //viewfactory tiré de la page http://www.androhub.com/android-textswitcher/
@@ -134,6 +141,14 @@ public class QuestionActivity extends AppCompatActivity{
                         }
                     });
         }
+        init();
+    }
+
+    //TODO trouver les autres choses a initialiser
+    public void init() {
+        gameEnded = false;
+        questionCount = -1;
+        goodAnswers = 0;
         newQuestion();
     }
 
@@ -178,13 +193,68 @@ public class QuestionActivity extends AppCompatActivity{
     }
 
     private void newQuestion(){
-        questionCount++;
+        if(questionCount >= questionsPerGame) {
+            gameEnded = true;
+            countDownTimer.cancel();
+            endDialog();
+        } else {
+            questionCount++;
 
-        //ajoute un delai ou on ne peut repondre a la question pour etre certain de ne
-        // pas avoir accrocher de bouton
-        new PreventClickCountDownTimer(preventTime, preventTime).start();
-        QuestionFetcher questionFetcher = new QuestionFetcher();
-        questionFetcher.execute();
+            //ajoute un delai ou on ne peut repondre a la question pour etre certain de ne
+            // pas avoir accrocher de bouton
+            new PreventClickCountDownTimer(preventTime, preventTime).start();
+
+            QuestionFetcher questionFetcher = new QuestionFetcher();
+            questionFetcher.execute();
+        }
+    }
+
+    //cré le dialog de fin de jeu et laffiche
+    private void endDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        View dialogView = (View)  getLayoutInflater().inflate(R.layout.single_play_game_end,null);
+        builder.setView(dialogView);
+        final AlertDialog endDialog = builder.create();
+
+        //felicitations
+        TextView felicitations = (TextView) dialogView.findViewById(R.id.end_felicitations);
+        String fel[] = getResources().getStringArray(R.array.game_end_felicitation);
+        felicitations.setText(fel[goodAnswers]);
+
+        TextView goodAnswersTv = (TextView) dialogView.findViewById(R.id.end_good_answers);
+        goodAnswersTv.setText(getString(R.string.goodAnswers) + ": " + goodAnswers);
+
+        ListView achievementsEarned = (ListView) dialogView.findViewById(R.id.end_achievements_earned);
+
+        //TODO get achievements earned programmatically
+        ArrayList<Achievement> achievements = new ArrayList<Achievement>();
+        achievements.add(new Achievement(0, "Answer 10 questions", 10, 10, 5, 10));
+        achievements.add(new Achievement(0, "Answer 10 questions", 10, 10, 5, 10));
+        achievements.add(new Achievement(0, "Answer 10 questions", 10, 10, 5, 10));
+
+        AchievementsAdapter adapter = new AchievementsAdapter(this,  achievements);
+        achievementsEarned.setAdapter(adapter);
+
+        Button replay = (Button) dialogView.findViewById(R.id.end_play_again_button_yes);
+        replay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                endDialog.cancel();
+                init();
+            }
+        });
+
+        Button dontReplay = (Button) dialogView.findViewById(R.id.end_play_again_button_no);
+        dontReplay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                endDialog.cancel();
+                finish();
+            }
+        });
+
+        endDialog.show();
+        endDialog.setCancelable(false);
     }
 
 
--- app/src/main/java/layout/CareerFragment.java
+++ app/src/main/java/layout/CareerFragment.java
@@ -1,18 +1,14 @@
 package layout;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 
+import com.app.game.quizee.AchievementsAdapter;
 import com.app.game.quizee.R;
 import com.app.game.quizee.backend.Achievement;
 
@@ -45,92 +41,9 @@ public class CareerFragment extends Fragment {
         achievements.add(new Achievement(0, "Answer 10 questions", 10, 10, 5, 10));
         achievements.add(new Achievement(0, "Answer 10 questions", 10, 10, 5, 10));
 
-        DetailsAdapter adapter = new DetailsAdapter(getActivity(),  achievements);
+        AchievementsAdapter adapter = new AchievementsAdapter(getActivity(),  achievements);
         statsList.setAdapter(adapter);
         achievementsList.setAdapter(adapter);
         return ll;
-    }
-
-    private static class ViewHolder {
-        TextView achievementName;
-        ImageView achievementIcon;
-        ImageView check;
-        ImageView currency;
-        TextView information;
-        ProgressBar pb;
-        TextView expGiven;
-        TextView goldGiven;
-    }
-
-    //Adapter inspiré de
-    // http://www.androidinterview.com/android-custom-listview-with-image-and-text-using-arrayadapter/
-    private class DetailsAdapter extends ArrayAdapter<Achievement> {
-
-        Activity context;
-
-        public DetailsAdapter (Activity context, ArrayList<Achievement> achievement) {
-            super(context, R.layout.contacts_item_list_layout, achievement);
-
-
-            this.context=context;
-        }
-
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
-            LayoutInflater inflater=context.getLayoutInflater();
-
-            ViewHolder holder;
-
-            Achievement a = getItem(position);
-
-            if(convertView == null) {
-                convertView = inflater.inflate(R.layout.achievements_item_list_layout, null, true);
-                holder = new ViewHolder();
-                holder.achievementName = (TextView) convertView.findViewById(R.id.achievement_item_name);
-                holder.achievementIcon = (ImageView) convertView.findViewById(R.id.contact_avatar_icon); //TODO remove?
-                holder.expGiven = (TextView) convertView.findViewById(R.id.achievement_exp_given);
-                holder.goldGiven = (TextView) convertView.findViewById(R.id.achievement_gold_given);
-                holder.check = (ImageView) convertView.findViewById(R.id.achievement_check);
-                holder.information = (TextView) convertView.findViewById(R.id.achievement_information);
-                holder.pb = (ProgressBar) convertView.findViewById(R.id.achievement_progress);
-                holder.currency = (ImageView) convertView.findViewById(R.id.currency_image);
-                convertView.setTag(holder);
-            } else {
-                holder = (ViewHolder) convertView.getTag();
-            }
-
-            holder.achievementName.setText(a.getDesc());
-            holder.information.setText(a.getInformation());
-            if(a.getXP() != 0) {
-                holder.expGiven.setVisibility(View.VISIBLE);
-                holder.expGiven.setText(getString(R.string.career_and) + " " + a.getXP() + " " + getString(R.string.career_xp));
-            } else {
-                holder.expGiven.setVisibility(View.INVISIBLE);
-            }
-            if(a.getMoney() != 0) {
-                holder.goldGiven.setVisibility(View.VISIBLE);
-                holder.currency.setVisibility(View.VISIBLE);
-                holder.goldGiven.setText(getString(R.string.career_gives) + " " + a.getMoney());
-            } else {
-                holder.goldGiven.setVisibility(View.INVISIBLE);
-                holder.currency.setVisibility(View.INVISIBLE);
-            }
-
-            if(a.getInformation() != null) {
-                holder.information.setText(a.getInformation());
-                holder.check.setVisibility(View.INVISIBLE);
-                holder.pb.setVisibility(View.INVISIBLE);
-            }   else {
-                if(a.getMaxProgress() == a.getProgress()) {
-                    holder.check.setVisibility(View.VISIBLE);
-                }
-                holder.check.setVisibility(View.INVISIBLE);
-                holder.pb.setVisibility(View.VISIBLE);
-                holder.pb.setProgress((a.getProgress() * 100)/ a.getMaxProgress());
-                holder.information.setText(a.getProgress() + " out of " + a.getMaxProgress());
-            }
-
-            return convertView;
-        }
     }
 }
