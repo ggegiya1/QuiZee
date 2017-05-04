ef4038e7dcb6d7e640ad451f9040995b294135c9
gegiya <gegiya@gmail.com>
2017-05-03 22:14:07 -0400

    more comments

--- app/src/main/java/com/app/game/quizee/layout/AchievementsAdapter.java
+++ app/src/main/java/com/app/game/quizee/layout/AchievementsAdapter.java
@@ -1,5 +1,6 @@
 package com.app.game.quizee.layout;
 import android.app.Activity;
+import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,15 +20,19 @@ import java.util.List;
 
 public class AchievementsAdapter extends ArrayAdapter<Achievement> {
 
-    Activity context;
+    private Activity context;
 
     public AchievementsAdapter (Activity context, List<Achievement> achievement) {
         super(context, R.layout.top_players_item_list_layout, achievement);
         this.context=context;
     }
 
+    /**
+     * Return a view for a row in the given position
+     */
+    @NonNull
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
         LayoutInflater inflater=context.getLayoutInflater();
 
         ViewHolder holder;
@@ -36,28 +41,38 @@ public class AchievementsAdapter extends ArrayAdapter<Achievement> {
 
         if(convertView == null) {
             convertView = inflater.inflate(R.layout.achievements_item_list_layout, null, true);
+            // initialise achievement view and elements
             holder = new ViewHolder();
             holder.achievementName = (TextView) convertView.findViewById(R.id.achievement_item_name);
             holder.expGiven = (TextView) convertView.findViewById(R.id.achievement_exp_given);
             holder.goldGiven = (TextView) convertView.findViewById(R.id.achievement_gold_given);
             holder.check = (ImageView) convertView.findViewById(R.id.achievement_check);
             holder.information = (TextView) convertView.findViewById(R.id.achievement_information);
+
+            // setup a progress bar to show the achievement progress
             holder.pb = (ProgressBar) convertView.findViewById(R.id.achievement_progress);
             holder.pb.setMax(100); // range 0 .. 100
             holder.currency = (ImageView) convertView.findViewById(R.id.currency_image);
             convertView.setTag(holder);
         } else {
+            // use already created view
             holder = (ViewHolder) convertView.getTag();
         }
 
+        // update achievement view content
+        // only show the relevant data, ignore empty, zero or null values
         holder.achievementName.setText(a.getDesc());
         holder.information.setText(a.getInformation());
+
+        // show gained experience
         if(a.getXP() != 0) {
             holder.expGiven.setVisibility(View.VISIBLE);
             holder.expGiven.setText(context.getString(R.string.career_and) + " " + a.getXP() + " " + context.getString(R.string.career_xp));
         } else {
             holder.expGiven.setVisibility(View.INVISIBLE);
         }
+
+        // show money earned
         if(a.getMoney() != 0) {
             holder.goldGiven.setVisibility(View.VISIBLE);
             holder.currency.setVisibility(View.VISIBLE);
@@ -67,6 +82,7 @@ public class AchievementsAdapter extends ArrayAdapter<Achievement> {
             holder.currency.setVisibility(View.INVISIBLE);
         }
 
+        // show the description
         if(a.getInformation() != null) {
             holder.information.setText(a.getInformation());
             holder.check.setVisibility(View.INVISIBLE);
--- app/src/main/java/com/app/game/quizee/layout/FavoriteCategorySelectionActivity.java
+++ app/src/main/java/com/app/game/quizee/layout/FavoriteCategorySelectionActivity.java
@@ -53,6 +53,11 @@ public class FavoriteCategorySelectionActivity extends AppCompatActivity impleme
     ImageView avatar;
     List<SelectableCategory> selectableCategories;
 
+
+    /**
+     * Create and initialise the activity view
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,23 +72,32 @@ public class FavoriteCategorySelectionActivity extends AppCompatActivity impleme
         setupUnselectButton();
     }
 
+    /**
+     * Clean up the player's selected categories
+     */
     @Override
     protected void onStart() {
         super.onStart();
         PlayerManager.getInstance().getCurrentPlayer().clearSelectedCategories();
     }
 
+
+    /**
+     * Start new single player game with selected questions once start button is pressed
+     */
     private void addStartButton(final Player player){
         Button startButton = (Button) findViewById(R.id.button_start);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // add selected categories to player
                 for (SelectableCategory sc: selectableCategories){
                     if (sc.isSelected()){
                         player.addSelectedCategory(sc.getCategory());
                     }
                 }
-                if (player.getCategoriesSelected().size()>0) {
+                // start game if at least one category has been selected
+                if (!player.getCategoriesSelected().isEmpty()) {
                     Intent intent = new Intent(v.getContext(), QuestionActivity.class);
                     startActivity(intent);
                 }else {
@@ -93,6 +107,9 @@ public class FavoriteCategorySelectionActivity extends AppCompatActivity impleme
         });
     }
 
+    /**
+     * Create categories list view
+     */
     private void addCategoriesList(){
         categoryList = (ListView) findViewById(R.id.category_list);
         categoryList.setChoiceMode(AbsListView.CHOICE_MODE_MULTIPLE);
@@ -100,7 +117,7 @@ public class FavoriteCategorySelectionActivity extends AppCompatActivity impleme
         for (Category c: PlayerManager.getInstance().getCurrentPlayer().getCategoriesFavorites()){
             selectableCategories.add(new SelectableCategory(c));
         }
-        // select categories by popularity descending
+        // sort categories by popularity in descending order
         Collections.sort(selectableCategories, new Comparator<SelectableCategory>() {
             @Override
             public int compare(SelectableCategory o1, SelectableCategory o2) {
@@ -114,11 +131,13 @@ public class FavoriteCategorySelectionActivity extends AppCompatActivity impleme
             public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
                 // get category associated to the current row
                 SelectableCategory category = selectableCategories.get(position);
+                // toggle selection on click
                 if(category.isSelected()) {
                     category.unSelect();
                 } else {
                     category.select();
                 }
+                // redraw the categories list
                 adapterCategory.notifyDataSetChanged();
             }
         });
@@ -130,7 +149,6 @@ public class FavoriteCategorySelectionActivity extends AppCompatActivity impleme
         level = (TextView) findViewById(R.id.level_main);
         avatar = (ImageView) findViewById(R.id.avatar);
         updatePlayerInfo(player);
-
     }
 
     private void updatePlayerInfo(Player player){
@@ -143,6 +161,12 @@ public class FavoriteCategorySelectionActivity extends AppCompatActivity impleme
         }
     }
 
+    /**
+     * Observer method called by player
+     * Updates all the display elements related to a player
+     * @param o Player instance
+     * @param arg not used
+     */
     @Override
     public void update(Observable o, Object arg) {
         // update player info if changed
@@ -191,6 +215,10 @@ public class FavoriteCategorySelectionActivity extends AppCompatActivity impleme
         CheckBox selectionToggle;
     }
 
+
+    /**
+     * Create a button to allow unselect all the categories
+     */
     public void setupUnselectButton() {
         Button unselect = (Button) findViewById(R.id.unselectButton);
         unselect.setOnClickListener(new View.OnClickListener() {
@@ -205,13 +233,18 @@ public class FavoriteCategorySelectionActivity extends AppCompatActivity impleme
     }
 
 
-
+    /**
+     * Stop music once the application goes in background
+     */
     @Override
     protected void onPause() {
         MusicService.ServiceBinder.getService().pauseMusic();
         super.onPause();
     }
 
+    /**
+     * Resume music once the application is back
+     */
     @Override
     protected void onResume() {
         MusicService.ServiceBinder.getService().resumeMusic(true);
